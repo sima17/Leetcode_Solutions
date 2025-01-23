27b767d4d1bdfcf91adbc6c27995fcf179e5a5b0
Siima <147436871+sima17@users.noreply.github.com>
2025-01-23 23:13:37 +0530

    Time: 3 ms (55.91%), Space: 26.8 MB (39.9%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+       int m = grid.size();
+       int n = grid[0].size();
+       int ans = 0;
+       vector<int>row(m,0);
+       vector<int>col(n,0);
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1){
+                row[i]+=1;
+                col[j]+=1;
+            }
+        }
+       }
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1 && (row[i]>1 || col[j]>1)){
+                ans++;
+            }
+        }
+       }
+       return ans;
+    }
+};
