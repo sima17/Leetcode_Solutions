e042bfa8c35d2bda7670115089a3147a94fa3101
Siima <147436871+sima17@users.noreply.github.com>
2025-01-24 12:39:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   bool isCycleDFS(vector<int> adj[],int u,vector<bool>& visited, vector<bool>& inRecursion){
+    visited[u]=true;
+    inRecursion[u]=true;
+
+    for(int &v: adj[u])
+   }
+  
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+    }
+};
